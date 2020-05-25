5a2a79e48c39db0d9605279a48e2392fcb44a2af
My Name <D_iliy@mail.ru>
2020-05-25 08:01:30 +0300

    finish loyaud

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,7 @@ module.exports={
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: isLoaders(),
+                use: jsLoaders(),
                 loader:{
                     loader: 'babel-loader',
                     options:{
